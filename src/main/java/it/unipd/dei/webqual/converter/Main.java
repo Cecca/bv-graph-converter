799894987bacd56b9e5f5d894d477588e7c6b98c
Matteo Ceccarello <matteo.ceccarello@gmail.com>
2013-09-26 15:37:12 +0200

    Add main

---
+++ src/main/java/it/unipd/dei/webqual/converter/Main.java
@@ -0,0 +1,41 @@
+package it.unipd.dei.webqual.converter;
+
+import it.unimi.dsi.big.webgraph.BVGraph;
+import it.unimi.dsi.big.webgraph.EFGraph;
+import it.unimi.dsi.big.webgraph.ImmutableGraph;
+import it.unimi.dsi.logging.ProgressLogger;
+
+import java.io.IOException;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    if (args.length != 2) {
+      System.err.println("Usage: bv-graph-converter links-file out-basename");
+      System.exit(1);
+    }
+
+    String inputFile = args[0];
+    String outBasename = args[1];
+
+    System.out.println("==== Loading graph from " + inputFile);
+    ImmutableGraph iag64 =
+      ImmutableAdjacencyGraph64.loadOffline(inputFile);
+
+
+    ProgressLogger pl = new ProgressLogger();
+    String efOut = outBasename + "-ef";
+    System.out.println(
+      "==== Converting the graph to Elias-Fano format: output" + efOut);
+    EFGraph.store(iag64, efOut, pl);
+
+    pl = new ProgressLogger();
+    String bvOut = outBasename + "-bv";
+    System.out.println(
+      "==== Converting the graph to Boldi-Vigna format: output" + bvOut);
+    ImmutableGraph efGraph = EFGraph.load(efOut);
+    BVGraph.store(efGraph, bvOut, pl);
+
+  }
+
+}
