bc3463789e2995f64249ae47f6fcba281e32aac8
Matteo Ceccarello <matteo.ceccarello@gmail.com>
2013-09-27 17:23:35 +0200

    Update main of Adjacency head iterator with some useful statistics

--- src/main/java/it/unipd/dei/webqual/converter/AdjacencyHeadIterator.java
+++ src/main/java/it/unipd/dei/webqual/converter/AdjacencyHeadIterator.java
@@ -2,10 +2,7 @@ package it.unipd.dei.webqual.converter;
 
 import java.io.*;
 import java.math.BigInteger;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.NoSuchElementException;
-import java.util.Set;
+import java.util.*;
 
 import static it.unipd.dei.webqual.converter.Utils.*;
 
@@ -18,6 +15,8 @@ public class AdjacencyHeadIterator implements Iterator<byte[]> {
 
   private boolean hasNext;
   private byte[] next;
+
+  private Collection<BigInteger> neighbours;
 
   private long count;
 
@@ -35,7 +34,12 @@ public class AdjacencyHeadIterator implements Iterator<byte[]> {
       throw new NoSuchElementException(
         "The first id is not the head of an adjacency list");
     }
-    this.next = reset(firstNode);
+    if(reset) {
+      this.next = reset(firstNode);
+    } else {
+      this.next = new byte[idLen];
+      System.arraycopy(firstNode, 0, next, 0, idLen);
+    }
 
     this.count = 1;
   }
@@ -45,16 +49,23 @@ public class AdjacencyHeadIterator implements Iterator<byte[]> {
     return hasNext;
   }
 
+  public Collection<BigInteger> neighbours() {
+    return neighbours;
+  }
+
   @Override
   public byte[] next() {
     final byte[] cur = next;
     // advance to the next head
     try {
       hasNext = false;
+      neighbours = new ArrayList<>();
       while (dis.available() > 0) {
         byte[] buf = new byte[idLen];
         dis.read(buf);
+//        System.out.print("Analyzing " + new BigInteger(buf).toString(16));
         if(isHead(buf)) {
+//          System.out.println("<<--------- Found a head");
           count++;
           if(reset) {
             next = reset(buf);
@@ -64,6 +75,9 @@ public class AdjacencyHeadIterator implements Iterator<byte[]> {
           }
           hasNext = true;
           break;
+        } else {
+          neighbours.add(new BigInteger(buf));
+//          System.out.println();
         }
       }
     } catch (IOException e) {
@@ -95,23 +109,36 @@ public class AdjacencyHeadIterator implements Iterator<byte[]> {
     AdjacencyHeadIterator it = new AdjacencyHeadIterator(
       "links.0", 16, false);
 
-    Set<BigInteger> heads = new HashSet<>();
+    Map<BigInteger, Collection<BigInteger>> heads = new HashMap<>();
+    long duplicates = 0;
+    long nonEmptyDuplicates = 0;
 
     while(it.hasNext()) {
       byte[] head = it.next();
       BigInteger bi = new BigInteger(head);
       if(bi.signum() >= 0) {
-        System.err.println("Head with first bit set to 0: " + bi.toString(16));
-        System.err.println("The first byte is: " + head[0]);
-        System.err.println("And isHead returns " + isHead(head));
+//        System.out.println("Head with first bit set to 0: " + bi.toString(16));
+//        System.out.println("The first byte is: " + head[0]);
+//        System.out.println("And isHead returns " + isHead(head));
         System.exit(1);
       }
-      boolean notContained = heads.add(bi);
-      if(!notContained) {
-        System.err.println("Duplicate element: " + bi.toString(16));
+      Collection<BigInteger> contained = heads.get(bi);
+      if(contained != null) {
+//        System.out.printf("Duplicate element: %s\n\tneighbours a: %s\n\tneighbours b: %s\n",
+//          bi.toString(16), contained.toString(), it.neighbours());
+        duplicates++;
+        if(it.neighbours().size() != 0) {
+          nonEmptyDuplicates++;
+        }
+      } else {
+        heads.put(bi, it.neighbours());
       }
     }
 
+    System.out.printf("Duplicates: %d / %d   -   %f %%\n",
+      duplicates, it.getCount(), (((double) duplicates) / it.getCount()) * 100);
+    System.out.printf("Of which non empty: %d / %d  -  %f %%\n",
+      nonEmptyDuplicates, duplicates, (((double) nonEmptyDuplicates) / duplicates) * 100);
 
   }
 
