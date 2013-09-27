c66b74fe42b7e5390f47009065375905b5c401c5
Matteo Ceccarello <matteo.ceccarello@gmail.com>
2013-09-27 15:21:17 +0200

    Start implementation of ImmutableAdjacencyGraph

---
+++ src/main/java/it/unipd/dei/webqual/converter/ImmutableAdjacencyGraph.java
@@ -0,0 +1,46 @@
+package it.unipd.dei.webqual.converter;
+
+import it.unimi.dsi.big.webgraph.ImmutableSequentialGraph;
+import it.unimi.dsi.sux4j.mph.MinimalPerfectHashFunction;
+
+import java.io.IOException;
+
+public class ImmutableAdjacencyGraph extends ImmutableSequentialGraph {
+
+  /** Length in bytes of the IDs */
+  private final int idLen;
+
+  /** The name of the file storing the graph. */
+  private final String filename;
+
+  /** The number of nodes of the graph. */
+  private final long numNodes;
+
+  private final MinimalPerfectHashFunction<byte[]> map;
+
+  public ImmutableAdjacencyGraph(CharSequence filename, int idLen) {
+    this.idLen = idLen;
+    this.filename = filename.toString();
+
+    try {
+      AdjacencyHeads heads = new AdjacencyHeads(this.filename, idLen);
+      MD5TransformationStrategy trStrat = new MD5TransformationStrategy();
+
+      this.map = new MinimalPerfectHashFunction<byte[]>(heads, trStrat);
+
+      this.numNodes = heads.getCount();
+
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private void mapAndCount() {
+
+  }
+
+  @Override
+  public long numNodes() {
+    return numNodes;
+  }
+}
