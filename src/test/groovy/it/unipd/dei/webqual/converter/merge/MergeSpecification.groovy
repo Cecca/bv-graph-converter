609ddda5bf9aebf0cbd349f864743c05d6c0d770
Matteo Ceccarello <matteo.ceccarello@gmail.com>
2013-10-24 11:15:33 +0200

    Add another test

--- src/test/groovy/it/unipd/dei/webqual/converter/merge/MergeSpecification.groovy
+++ src/test/groovy/it/unipd/dei/webqual/converter/merge/MergeSpecification.groovy
@@ -18,6 +18,13 @@ class MergeSpecification extends Specification {
     }
   }
 
+  class ArrMerger implements Merger<byte[]> {
+    @Override
+    byte[] merge(byte[] first, byte[] second) {
+      return first
+    }
+  }
+
   def "Merged iterators are sorted" () {
     setup:
     def a = [1,2,3,4,5,6,7,8]
@@ -48,6 +55,47 @@ class MergeSpecification extends Specification {
 
     then:
     merged == [1,2,3,4,5,6,7,8,9,10,11]
+  }
+
+  def "Merged iterators contain no duplicates with byte arrays" () {
+    setup:
+    def rnd = new Random()
+    def first = []
+    for(int i=0; i<10; i++) {
+      byte[] b = new byte[16]
+      rnd.nextBytes(b)
+      first.add(b)
+    }
+    def second = []
+    for(int i=0; i<10; i++) {
+      byte[] b = new byte[16]
+      rnd.nextBytes(b)
+      second.add(b)
+    }
+    def lit = new LazyMergeIterator<byte[]>(first.iterator(), second.iterator(), new ArrayComparator(), new ArrMerger())
+
+    when:
+    def merged = []
+    while(lit.hasNext()) {
+      merged.add(lit.next())
+    }
+
+    then:
+    containsNoDuplicates(merged)
+  }
+
+  def static containsNoDuplicates(merged) {
+    ArrayComparator cmp = new ArrayComparator();
+    Iterator<byte[]> it = merged.iterator();
+    byte[] last = it.next();
+    while(it.hasNext()) {
+      byte[] cur = it.next();
+      if(cmp.compare(cur, last) == 0) {
+        return false
+      }
+      last = cur;
+    }
+    return true
   }
 
   def "Composed iterators yield the correct result an odd number of iterators" () {
